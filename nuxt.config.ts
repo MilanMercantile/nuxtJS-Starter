46a0b46d87b25ac2c9a70df82562727a5a75b434
MilanMercantile <135330024+MilanMercantile@users.noreply.github.com>
2024-05-22 11:19:19 +0545

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt3'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  nitro: {
-    awsAmplify: {
-      // catchAllStaticFallback: true,
-      // imageOptimization: { "/_image", cacheControl: "public, max-age=3600, immutable" },
-      // imageSettings: { ... },
-    }
+  nitro:{
+    preset:'awsAmplify'
   }
 })

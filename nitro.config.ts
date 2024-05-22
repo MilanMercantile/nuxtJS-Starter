ea2b2ad436ac3be320d934829457ec6a0e62a2b8
MilanMercantile <135330024+MilanMercantile@users.noreply.github.com>
2024-05-22 10:59:19 +0545

    Create nitro.config.ts

---
+++ nitro.config.ts
@@ -0,0 +1,7 @@
+export default defineNitroConfig({
+  awsAmplify: {
+      // catchAllStaticFallback: true,
+      // imageOptimization: { path: "/_image", cacheControl: "public, max-age=3600, immutable" },
+      // imageSettings: { ... },
+  }
+})
